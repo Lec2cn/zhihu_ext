0cc645062772552b0e604921f4db7ec8a37b97de
Lec2cn <583646461@qq.com>
2014-02-07 20:52:44 +0800

    导航栏改造完成
    Leckie

--- core.js
+++ core.js
@@ -5,34 +5,57 @@
   var top_logo = $("#zh-top-link-logo");
   var timeline_border = $("div.feed-item div.feed-item");
   var timeline_card = $("div.feed-item-inner");
+  var top_nav_list = $("ul.zu-top-nav-ul")[0];
 
 
   var new_userinfo_data = top_nav_userinfo.children(0);
-  // var user_avatar = $("div.zm-profile-header-avatar-container self").children();
-  var new_userinfo = document.createElement("div");
-  new_userinfo.setAttribute("class","top-nav-userinfo");
-  new_userinfo.id = "ext_top-nav-userinfo";
-  new_userinfo.innerHTML = "<span>" + new_userinfo_data[0].innerHTML + "</span> <img src='"+new_userinfo_data[1].src+"'>";
+  var new_setting_list = top_nav_userinfo.children(1);
 
+  var new_userinfo = document.createElement("div");
+  // var new_setting_list_div = $("<div></div>");
+
+  new_userinfo.setAttribute("class","top-nav-userinfo");
+  // new_setting_list_div.attr("class","top-nav-list");
+
+  new_userinfo.id = "ext_top-nav-userinfo";
+  // new_setting_list_div.id = "top-nav-list";
+  new_userinfo.innerHTML = "<span>" + new_userinfo_data[0].innerHTML + "</span> <img src='"+new_userinfo_data[1].src+"'>";
+  // new_setting_list_div.html("<ul id='navlist-ul'></ul>");
 
   top_nav.removeClass();
   top_nav_profile.removeClass();
   top_logo.removeClass();
-  // timeline_border.removeClass();
   timeline_card.removeClass();
   top_nav_userinfo.remove();
 
 
   top_nav.attr("class","zu-top-new");
   top_nav_profile.attr("class","top-nav-profile");
+  top_nav_profile.attr("id","ext-top-nav-profile");
   timeline_border.attr("class",".feed-item");
   top_logo.attr("class","top-link-logo");
   timeline_card.attr("class","new-feed-item-inner");
 
 
   $(document).on("click", "#ext_top-nav-userinfo" , jumptoself(top_nav_userinfo[0].href));
-  
   top_nav_profile.append(new_userinfo);
+
+  // $( "#ext-top-nav-profile" ).hover(
+  //   function() {
+  //     $("#ext-exit-btn").css({display: "block"});
+  //     // $("#ext-top-nav-profile").css({display: "none"});
+  //   }, function() {
+  //     // $("#ext-exit-btn").css({display: "none"});
+  //     // $("#ext-top-nav-profile").css({display: "block"});
+  //   }
+  // );
+
+
+  $(top_nav_list).append($("<li class='zu-top-nav-li'><a class='zu-top-nav-link' href='/inbox'>私信</a></li>"));
+  $(top_nav_list).append($("<li class='zu-top-nav-li'><a class='zu-top-nav-link' href='/settings'>设置</a></li>"));
+  $(top_nav_list).append($("<li class='zu-top-nav-li'><a class='zu-top-nav-link' href='/logout'>退出</a></li>"));
+  // $(top_nav).append($("<div id='ext-exit-btn' class='ext-exit-btn'>退出</div>"));
+  // $("#ext-exit-btn").css({width: $(top_nav_profile).width() });
 
 
   function jumptoself(href){
@@ -57,5 +80,4 @@
   //     console.log("123");
   //   }
   // }
-
 
